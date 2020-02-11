df1310c23ae0329e68a2ca9e19668d5c497e5481
dannyLiu <h152074716@gmail.com>
2020-02-11 16:34:19 +0800

    繳交作業1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { element } from 'protractor';
+import { escape } from 'querystring';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +11,21 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+
+  textlength = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+  keyin( $event) {
+    console.log($event);
+    this.textlength = $event.target.value.length;
+    if ($event.key === 'Escape') {
+      this.textlength = 0;
+      $event.target.value = '';
+     }
   }
 }
